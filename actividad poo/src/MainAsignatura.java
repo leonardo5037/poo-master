de60babd2521970ed0dee4e4c0fe1015ec2c4d2f
Leonardo Garcia <leonardogarciaponce2006@gmail.com>
2025-02-19 08:59:09 -0600

    actividades

--- actividad poo/src/MainAsignatura.java
+++ actividad poo/src/MainAsignatura.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.sql.SQLOutput;
 
 public class MainAsignatura {
+    public static void main(String[] args) {
+        Asignatura poo = new Asignatura();
+        poo.setNombre("Programacion orientada a objetos");
+        poo.setClave("AED-1286");
+        poo.setCreditos(5);
+        poo.setTeoricas(2);
+        poo.setPracticas(3);
+
+        System.out.println(poo);
+
+    }
 }
--- actividad poo/src/MainPersonaArrays.java
+++ actividad poo/src/MainPersonaArrays.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainPersonaArrays {
+    public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+    ArrayList<String> lista = new ArrayList();
+    String frase="";
+    System.out.println("Escribe para salir");
+    while (true){
+        System.out.println("Dame una frase: ");
+        frase = teclado.nextLine();
+        if (frase.equals("quit")){
+            lista.add(frase);
+            break;
+        }
+        for (String s: lista) {
+            System.out.println(s);
+        }
+
+    }
+
+    }
 }
+
+    /*
+    public static void main(String[] args) {
+        Persona p = new Persona();
+        ArrayList <String> lista = new ArrayList();
+
+        String frase = "Feliz San Valentine";
+        lista.add(frase);
+
+        frase = "Que reciban muchos apapachos";
+
+        lista.add(frase);
+
+        frase = "Hoy sera un dia maravilloso... para otros";
+
+        System.out.println(frase);
+        System.out.println(lista.get(0));
+        System.out.println(lista.get(1));
+
+        if (lista.contains(frase)){
+            System.out.println("Tristemente asi sera");
+        }else{
+            System.out.println("Eres afortunado");
+        }
+        System.out.println(lista.size());
+
+    } */
+
